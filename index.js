76953ee2045116b504b73f14c469c282b2670096
Ludi <ludi.adressemail@gmail.com>
2023-05-09 13:51:10 +0200

    add value of the imput in the headerCart

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 // cart
 
 let cartBtn = document.getElementById("btnCart");
-cartBtn.addEventListener('click', addToCart)
+cartBtn.addEventListener('click', addToCart);
+
+let cartHeader = document.getElementById("cartHeader");
 
 let cartNb = document.getElementById("cartNb");
 
-let sumCart = 1;
+let sumCart = cartNb.value;
 
 function addToCart(event) {
-    cartNb.textContent = sumCart++;
+    cartHeader.textContent = sumCart++;
+   }
+
+
+// accordeons
+
+let btnAvantages = document.getElementById("btnProductDescription");
+btnAvantages.addEventListener('click', switchCLass);
+
+let ulProductDescription = document.getElementById("removeUl")
+
+function switchCLass(event) {
+    if (ulProductDescription.classList.contains('remove')) {
+        ulProductDescription.classList.remove('remove');
+    }
+    else {
+        ulProductDescription.classList.add("remove");
+    }  
 }
+
+
+// let btnCaracteristiques = document.getElementById("btnProductFeatures");
+// btnCaracteristiques.addEventListener('click', changeCLass)
+
+// let dtProductFeatures = document.getElementById("removeFeatures")
+// console.log(dtProductFeatures);
+
+// function changeCLass(event) {
+//     dtProductFeatures.classList.toggle("remove");
+// }
+
+    
